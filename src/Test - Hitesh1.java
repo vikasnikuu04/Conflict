425e18a787b26b57b323349a9c95f2872eafbd64
vikasnikuu04 <45723524+vikasnikuu04@users.noreply.github.com>
2020-05-01 16:38:17 -0400

    Update Test - Hitesh1.java

--- src/Test - Hitesh1.java
+++ src/Test - Hitesh1.java
@@ -2,8 +2,8 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Test - master change");
-		System.out.println("Test - test2 hitesh.java");
+		System.out.println("stash master teste");
+		System.out.println("stash master test");
 
 	
 	}
